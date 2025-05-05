25e3ca77f591864c9fb911bad1947bf3b058115c
Rubén García-Patos Benito <ruben.gbenito@alumnos.upm.es>
2025-05-05 19:15:30 +0200

    fix: delete server file

--- lib/spe.ex
+++ lib/spe.ex
@@ -1,18 +1,21 @@
 defmodule Spe do
-  @moduledoc """
-  Documentation for `Spe`.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  def start_link(opts), do: GenServer.start_link(__MODULE__, opts)
+  def submit_job(pid, job), do: GenServer.call(pid, :submit_job)
 
-  ## Examples
+  def init(opts) do
+    num_workers = Keyword.get(opts, :num_workers, :unbounded)
+    {:ok, %{num_workers: num_workers, current_workers: 0, pid_queued_jobs: [], jobs: %{}}}
+  end
 
-      iex> Spe.hello()
-      :world
+  def handle_call({:submit_job, %{"name" => name, "tasks" => tasks} = job}, _from, state) do
+    job_id = 0
+    {:reply, {:ok, job_id}, %{state | "jobs" => %{state["jobs"] | job_id => job}}}
+  end
 
-  """
-  def hello do
-    :world
+  def handle_call({:submit_job, _bad_job}, _from, state) do
+    response = {:error, "Not name or tasks keys in the job"}
+    {:reply, response, state}
   end
 end
