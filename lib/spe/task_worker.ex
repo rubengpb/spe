c35ed74655624c3c422a06a8fab2581644912a8c
Alejandro Soriano <alejandro.scompta@alumnos.upm.es>
2025-05-27 16:49:24 +0200

    Refactor task worker

--- lib/spe/task_worker.ex
+++ lib/spe/task_worker.ex
@@ -1,65 +1,54 @@
 defmodule SPE.TaskWorker do
-  def start_link(name, fun, input, timeout, caller) do
-    Task.start(fn -> run(name, fun, input, timeout, caller) end)
+  def start_link(name, function, input, timeout, caller) do
+    Task.start(fn -> run(name, function, input, timeout, caller) end)
   end
 
-  defp run(name, fun, input, timeout, caller) do
+  defp run(name, function, input, timeout, caller) do
     result =
       case timeout do
-        :infinity ->
-          execute_without_timeout(fun, input)
+        :infinity -> execute_without_timeout(function, input)
 
-        ms when is_integer(ms) ->
-          execute_with_timeout(fun, input, ms)
+        ms when is_integer(ms) -> execute_with_timeout(function, input, ms)
       end
 
     send(caller, {:task_finished, name, result})
   end
 
-  defp execute_without_timeout(fun, input) do
-    id = make_ref()
-    parent = self()
-
-    {pid, ref} =
-      spawn_monitor(fn ->
-        result = run_safe(fun, input)
-        send(parent, {id, result})
-      end)
-
-    receive do
-      {^id, result} ->
-        result
-
-      {:DOWN, ^ref, :process, ^pid, reason} ->
-        {:failed, {:crashed, reason}}
-    end
-  end
-
-  defp execute_with_timeout(fun, input, ms) do
-    task = Task.async(fn -> execute_without_timeout(fun, input) end)
+  defp execute_with_timeout(function, input, ms) do
+    task = Task.async(fn -> execute_without_timeout(function, input) end)
 
     case Task.yield(task, ms) do
-      {:ok, result} ->
-        result
-
-      {:exit, reason} ->
-        {:failed, {:crashed, reason}}
-
+      {:ok, result} -> result
+      {:exit, reason} -> {:failed, {:crashed, reason}}
       nil ->
         Task.shutdown(task, :brutal_kill)
         {:failed, :timeout}
     end
   end
 
-  defp run_safe(fun, input) do
+  defp execute_without_timeout(function, input) do
+    id = make_ref()
+    parent = self()
+
+    {pid, ref} =
+      spawn_monitor(fn ->
+        result = safe_execution(function, input)
+        send(parent, {id, result})
+      end)
+
+    receive do
+      {^id, result} -> result
+      {:DOWN, ^ref, :process, ^pid, reason} -> {:failed, {:crashed, reason}}
+    end
+  end
+
+  defp safe_execution(function, input) do
     try do
-      {:result, fun.(input)}
+      {:result, function.(input)}
     rescue
-      e ->
-        {:failed, {:crashed, Exception.message(e)}}
+      e -> {:failed, {:crashed, Exception.message(e)}}
     catch
-      _kind, error ->
-        {:failed, {:crashed, error}}
+      _kind, error -> {:failed, {:crashed, error}}
     end
   end
 end
